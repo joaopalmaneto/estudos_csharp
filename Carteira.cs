1cb3851d2c5b85aa6b32415a5af792a80ad23b51
Joao Palma <jpalmaneto@gmail.com>
2020-06-21 01:11:48 -0300

    21 jun

--- Carteira.cs
+++ Carteira.cs
@@ -1,16 +1,28 @@
 using System; 
+using System.Collections.Generic;
+
 
 namespace gerenciador{
 
     class Carteira
     {
         double capitalInicial;
+        double ValorPresente;
+        List<double> entradas = new List<double>(); 
+        List<double> saidas = new List<double>();
         
         public Carteira(double capitalInicial){
             this.capitalInicial = capitalInicial;
         }
         public void adicionarReceita(){
-            Console.WriteLine("adicionar receita");
+            Console.WriteLine("\nAdicionar receita");
+
+            while(true){
+                if(Utilidades.converterEntradaDouble() != null){
+                    entradas.Add()
+                }
+            }
+
         }
 
         public void adicionarDespesa(){
